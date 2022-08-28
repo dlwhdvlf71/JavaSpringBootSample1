f30e0507386981dc95d556e973e0b17c7d098f1d
Anony <dlwhdvlf71@gmail.com>
2022-08-29 00:32:56 +0900

    First Commit

---
+++ src/test/java/ano/anodemo/AnoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ano.anodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
